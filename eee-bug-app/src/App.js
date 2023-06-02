ffa95cb2ea553a54593da6d8ec0f1267ee1c485c
Ezra Reich <64613180+EZePiZy@users.noreply.github.com>
2023-06-02 20:36:08 +0100

    feat: getting coordinates from DB; WORK IN PROGRESS

--- eee-bug-app/src/App.js
+++ eee-bug-app/src/App.js
@@ -7,100 +7,108 @@ const HEIGHT = 15,
   RECT_SIZE = Math.min(window.innerHeight, window.innerWidth) / Math.max(HEIGHT, WIDTH);
 
 const _range = (n) => [...Array(n).keys()];
+const coordinates = {};
 
-function useData(){
-  const [maze, updateMaze] = useState([[]]);
+
+function useData() {
+  const [NID, setNID] = useState(null);
+  const [X_Coord, setXCoord] = useState(null);
+  const [Y_Coord, setYCoord] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+  
+
   React.useEffect(() => {
-  fetch("http://localhost:3001/mazeQuery/")
-    .then((res) => res.json())
-    .then((data) => alert(JSON.stringify(data)))
-    .catch((err) => alert(err));
-}, []);
-
-  return maze;
+    fetch("http://localhost:3001/mazeQuery/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.length)
+        for(let i=0; i<data.length; i++){
+            setNID(data[i].NID);
+            setXCoord(data[i].X_Coord);
+            setYCoord(data[i].Y_Coord);
+            setCoordinates({ x: data[i].X_Coord, y: data[i].Y_Coord });
+        }
+      })
+      .catch((err) => alert(err));
+  }, []);
+  console.log(coordinates)
+  return { NID, X_Coord, Y_Coord, coordinates };
 }
 
 function MazeComponent() {
-  const maze = useData();
-  let data = JSON.stringify(maze)
-  console.log(data)
+  const { NID, X_Coord, Y_Coord } = useData();
 
-  // Rest of the component's code
 
-  return <div>{JSON.stringify(maze)}</div>;
+  return (
+    <div>
+      <div>NID: {NID}</div>
+      <div>X_Coord: {X_Coord}</div>
+      <div>Y_Coord: {Y_Coord}</div>
+    </div>
+  );
 }
 
-
-
-
-const _generate = (height, width) => {
-  
-  // React.useEffect(() => {
-  //   fetch("http://localhost:3001/mazeQuery/")
-  //     .then((res) => res.json())
-  //     .then((data) => alert(JSON.stringify(data)))
-  //     .catch((err) => alert(err));
-  // }, []);
-
-  const maze = {};
-
-  _range(height).forEach((y) => {
-    _range(width).forEach((x) => {
-      maze[`${x}-${y}`] = Math.random() < 0.3;
-    });
-  });
-
-  return maze;
-};
-
-class Maze extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      height: HEIGHT,
-      width: WIDTH,
-      maze: _generate(HEIGHT, WIDTH)
-    };
-
-    this.handleClickGenerate = this.handleClickGenerate.bind(this);
-  }
-
-  handleClickGenerate(event) {
-    this.setState({ maze: _generate(HEIGHT, WIDTH) });
-  }
-
-  render() {
-    return (
-      <div>
-        <Stage width={WIDTH * RECT_SIZE} height={HEIGHT * RECT_SIZE}>
-          <Layer>
-            {_range(this.state.height).map((y) =>
-              _range(this.state.width).map((x) => (
-                <Rect
-                  key={`${x}-${y}`}
-                  x={x * RECT_SIZE}
-                  y={y * RECT_SIZE}
-                  width={RECT_SIZE}
-                  height={RECT_SIZE}
-                  fill={this.state.maze[`${x}-${y}`] ? "grey" : "white"}
-                  stroke="black"
-                  strokeWidth={1}
-                />
-              ))
-            )}
-          </Layer>
-        </Stage>
-        <input
-          type="button"
-          value="Regenerate"
-          onClick={this.handleClickGenerate}
-        />
-      </div>
-    );
-  }
-}
 
 // export default App;
-export default function App() {
-  return <Maze />, <MazeComponent />;
-}
+export default MazeComponent;
+
+
+
+
+// const _generate = (height, width, x, y) => {
+//   const maze = {};
+//   _range(height).forEach((y) => {
+//     _range(width).forEach((x) => {
+//       maze[`${x}-${y}`] = Math.random() < 0.3;
+//     });
+//   });
+
+//   return maze;
+// };
+
+// class Maze extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       height: HEIGHT,
+//       width: WIDTH,
+//       maze: _generate(HEIGHT, WIDTH)
+//     };
+
+//     this.handleClickGenerate = this.handleClickGenerate.bind(this);
+//   }
+
+//   handleClickGenerate(event) {
+//     this.setState({ maze: _generate(HEIGHT, WIDTH) });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <Stage width={WIDTH * RECT_SIZE} height={HEIGHT * RECT_SIZE}>
+//           <Layer>
+//             {_range(this.state.height).map((y) =>
+//               _range(this.state.width).map((x) => (
+//                 <Rect
+//                   key={`${x}-${y}`}
+//                   x={x * RECT_SIZE}
+//                   y={y * RECT_SIZE}
+//                   width={RECT_SIZE}
+//                   height={RECT_SIZE}
+//                   fill={this.state.maze[`${x}-${y}`] ? "grey" : "white"}
+//                   stroke="black"
+//                   strokeWidth={1}
+//                 />
+//               ))
+//             )}
+//           </Layer>
+//         </Stage>
+//         <input
+//           type="button"
+//           value="Regenerate"
+//           onClick={this.handleClickGenerate}
+//         />
+//       </div>
+//     );
+//   }
+// }
