1473149025a69c57eb9b771fae0aae69f4a99b73
Ezra Reich <64613180+EZePiZy@users.noreply.github.com>
2023-06-05 18:12:00 +0100

    feat: Fetching nodes coordinates from AWS DB & display on frontend

--- eee-bug-app/src/App.js
+++ eee-bug-app/src/App.js
@@ -1,135 +1,46 @@
-import React, { Component, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { useCanvas } from "./Canvas";
 
 
 
 function useData() {
-  const [NID, setNID] = useState([]);
-  const [X_Coord, setXCoord] = useState([]);
-  const [Y_Coord, setYCoord] = useState([]);
+ 
   const [coordinates, setCoordinates] = useState([]);
-  var myCoord = []
 
-
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("http://localhost:3001/mazeQuery/")
       .then((res) => res.json())
       .then((data) => {
-        setNID(data.map(item => item.NID));
-        setXCoord(data.map(item => item.X_Coord));
-        setYCoord(data.map(item => item.Y_Coord));
         setCoordinates(data.map(item => ({ x: item.X_Coord, y: item.Y_Coord })));
       })
       .catch((err) => alert(err));
   }, []);
   const coordData = coordinates.length > 0 ? coordinates : [];
-  return [ NID, X_Coord, Y_Coord, coordData ];
+  return [ coordData ];
 }
 
+
 function MazeComponent() {
-  const [ NID, X_Coord, Y_Coord, coordData ] = useData();
-  console.log(coordData)
-  const [coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight] = useCanvas();
-
-
-
-  const handleCanvasClick = (event) => {
-    // on each click get current mouse location 
-    const currentCoord = { x: event.clientX, y: event.clientY };
-    // add the newest mouse location to an array in state 
-    setCoordinates([...coordinates, currentCoord]);
-  };
-
-  const handleClearCanvas = () => {
-    setCoordinates([]);
-  };
+  const canvasWidth = window.innerWidth * 0.85;
+  const canvasHeight = window.innerHeight * 0.85;
+  
+  const [coordData] = useData();
+  // console.log(coordData)
+  const canvasRef = useCanvas(coordData, canvasWidth, canvasHeight);
 
   return (
     <main className="App-main" >
-      {/* <canvas
+      <canvas
         className="App-canvas"
         ref={canvasRef}
         width={canvasWidth}
         height={canvasHeight}
-        onClick={handleCanvasClick} />
-      <div className="button" >
-        <button onClick={handleClearCanvas} > CLEAR </button>
-      </div> */}
-      <div>
-      {coordData.map((item, index) => (
-        
-        <div key={index}>
-          <p>X_Coord: {item.x}</p>
-          <p>Y_Coord: {item.y}</p>
-        </div>
-      ))}
-    </div>
+        />
     </main>
   );
 }
 
-
-// export default App;
 export default MazeComponent;
 
 
-
-
-// const _generate = (height, width, x, y) => {
-//   const maze = {};
-//   _range(height).forEach((y) => {
-//     _range(width).forEach((x) => {
-//       maze[`${x}-${y}`] = Math.random() < 0.3;
-//     });
-//   });
-
-//   return maze;
-// };
-
-// class Maze extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       height: HEIGHT,
-//       width: WIDTH,
-//       maze: _generate(HEIGHT, WIDTH)
-//     };
-
-//     this.handleClickGenerate = this.handleClickGenerate.bind(this);
-//   }
-
-//   handleClickGenerate(event) {
-//     this.setState({ maze: _generate(HEIGHT, WIDTH) });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Stage width={WIDTH * RECT_SIZE} height={HEIGHT * RECT_SIZE}>
-//           <Layer>
-//             {_range(this.state.height).map((y) =>
-//               _range(this.state.width).map((x) => (
-//                 <Rect
-//                   key={`${x}-${y}`}
-//                   x={x * RECT_SIZE}
-//                   y={y * RECT_SIZE}
-//                   width={RECT_SIZE}
-//                   height={RECT_SIZE}
-//                   fill={this.state.maze[`${x}-${y}`] ? "grey" : "white"}
-//                   stroke="black"
-//                   strokeWidth={1}
-//                 />
-//               ))
-//             )}
-//           </Layer>
-//         </Stage>
-//         <input
-//           type="button"
-//           value="Regenerate"
-//           onClick={this.handleClickGenerate}
-//         />
-//       </div>
-//     );
-//   }
-// }
--- eee-bug-app/src/Canvas.js
+++ eee-bug-app/src/Canvas.js
@@ -1,50 +1,35 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-// Path2D for a Heart SVG
-const heartSVG = "M0 200 v-200 h200 a100,100 90 0,1 0,200 a100,100 90 0,1 -200,0 z"
-const SVG_PATH = new Path2D(heartSVG);
 
-// Scaling Constants for Canvas
-const SCALE = 0.1;
-const OFFSET = 80;
-export const canvasWidth = window.innerWidth * 0.85;
-export const canvasHeight = window.innerHeight * 0.85;
-
-export function draw(ctx, location) {
-    console.log("attempting to draw")
-    ctx.fillStyle = 'red';
-    ctx.shadowColor = 'blue';
-    ctx.shadowBlur = 15;
-    ctx.save();
-    ctx.scale(SCALE, SCALE);
-    ctx.translate(location.x / SCALE - OFFSET, location.y / SCALE - OFFSET);
-    ctx.rotate(225 * Math.PI / 180);
-    ctx.fill(SVG_PATH);
-    // .restore(): Canvas 2D API restores the most recently saved canvas state
-    ctx.restore();
+export function draw(ctx, coordinates){
+  console.log("attempting to draw")
+  ctx.fillStyle = 'red';
+  coordinates.forEach((coordinate) => {
+    ctx.beginPath();
+    ctx.arc(coordinate.x, coordinate.y, 5, 0, 2 * Math.PI);
+    ctx.fill();
+  });
 };
 
-export function useCanvas() {
+
+export function useCanvas(jsonData, canvasWidth, canvasHeight) {
     const canvasRef = useRef(null);
-    const [coordinates, setCoordinates] = useState([]);
-
     useEffect(() => {
-        const canvasObj = canvasRef.current;
-        if(canvasObj) {
-            const ctx = canvasObj.getContext('2d');
-            if (ctx) {
-            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-            // draw all coordinates held in state
-            coordinates.forEach((coordinate) => { draw(ctx, coordinate) });
-            } else {
-                console.log("getContext() returned null")
-            } 
+      const canvasObj = canvasRef.current;
+      if (canvasObj) {
+        const ctx = canvasObj.getContext('2d');
+        if (ctx) {
+          ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+          draw(ctx, jsonData);
         } else {
-            console.log("canvasRef.current is null")
+          console.log("getContext() returned null");
         }
-
-      }, [coordinates]);
-
-
-    return [coordinates, setCoordinates, canvasRef, canvasWidth, canvasHeight];
-}
+      } else {
+        console.log("canvasRef.current is null");
+      }
+    }, [jsonData, canvasWidth, canvasHeight]);
+  
+    return canvasRef;
+  }
+    
+  
