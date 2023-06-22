24e4064e21eadfe0b4a94a4e8ff128cb22c48a1b
Diego Van Overberghe <36131808+Diegovano@users.noreply.github.com>
2023-06-22 04:53:12 +0100

    feat: websocket broken or maybe the server part is fine?

--- backend_server/index.js
+++ backend_server/index.js
@@ -6,6 +6,29 @@ const PORT = process.env.NODE_PORT || 3001;
 const path = require('path')
 const app = express();
 
+const { WebSocketServer, WebSocket } = require('ws'); // websocket for vehicle navigation
+
+try {
+  const wss = new WebSocketServer({ port: 8080 });
+  wss.on('connection', function connection(ws) {
+    ws.on('error', console.error);
+    
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+    
+    ws.send('something');
+    });
+} catch (error) {
+  console.log(`Error creating websocket server: ${error.message}`);
+}
+
+// const ws1 = new WebSocket("ws://127.0.0.1:8080");
+
+// ws1.on("open", () => {
+//   ws1.send("hello local!");
+// });
+
 app.use(express.static(path.resolve(__dirname, './eee-bug-app/build')));
 
 app.use(bodyParser.json());
@@ -28,8 +51,13 @@ var con = mysql.createConnection(
         database: "MazeDB"
     });
 con.connect(function (err) {
-    if (err) throw err;
-    console.log("Successfully connected to the database...\n");
+    if (err) {
+      console.log("Successfully failed to connect to the database...\n");
+      // throw err;
+    }
+    else {
+      console.log("Successfully connected to the database...\n");
+    }
 });
 
 app.get("/mazeQuery", (_req, res) => {
